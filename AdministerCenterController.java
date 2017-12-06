29b15822d106758b3993f5f1dd52664ba410e003
schrius <schriusty@gmail.com>
2017-12-06 01:36:53 -0500

    Add table column. Unfinised JDBC
    Add table column. Unfinised JDBC

--- AdministerCenterController.java
+++ AdministerCenterController.java
@@ -80,7 +80,7 @@ public class AdministerCenterController {
 	private RadioButton instructorRadio;
 	@FXML
 	private RadioButton studentRadio;
-	
+
 	@FXML
 	private TableView<Object> table;
 
@@ -107,73 +107,73 @@ public class AdministerCenterController {
 	public void searchButtonListerner() throws ClassNotFoundException, SQLException{
 		String search = searchField.getText();
 		String sql;
-		
+
 		if(courseRadio.isSelected()){
 			sql = "SELECT * FROM Course WHERE CourseName = '" + search + "' OR " + "CourseCode = '" + search + "'";
 			RegisterJDBC.excuteSQL(sql);
 			TableView<Student> studentTable = new TableView<Student>();
-			
+
 			TableColumn<Student, String> idColumn = new TableColumn<>("Student ID");
 			idColumn.setMinWidth(100);
 			idColumn.setCellValueFactory(new PropertyValueFactory<>("studentID"));
-			
+
 			TableColumn<Student, String> firstNameColumn = new TableColumn<>("FirstName");
 			firstNameColumn.setMinWidth(100);
 			firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-			
+
 			TableColumn<Student, String> lastNameColumn = new TableColumn<>("LastName");
 			lastNameColumn.setMinWidth(100);
 			lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-			
+
 			TableColumn<Student, String> addressColumn = new TableColumn<>("Address");
 			addressColumn.setMinWidth(100);
 			addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
-			
+
 			TableColumn<Student, String> cityColumn = new TableColumn<>("City");
 			cityColumn.setMinWidth(100);
 			cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
-			
+
 			TableColumn<Student, String> stateColumn = new TableColumn<>("State");
 			stateColumn.setMinWidth(100);
 			stateColumn.setCellValueFactory(new PropertyValueFactory<>("state"));
-			
+
 			TableColumn<Student, String> zipCodeColumn = new TableColumn<>("ZipCode");
 			zipCodeColumn.setMinWidth(100);
 			zipCodeColumn.setCellValueFactory(new PropertyValueFactory<>("zipcode"));
-			
+
 			TableColumn<Student, String> SSNColumn = new TableColumn<>("SSN");
 			SSNColumn.setMinWidth(100);
 			SSNColumn.setCellValueFactory(new PropertyValueFactory<>("SSN"));
-			
+
 			TableColumn<Student, String> DOBColumn = new TableColumn<>("DOB");
 			DOBColumn.setMinWidth(100);
 			DOBColumn.setCellValueFactory(new PropertyValueFactory<>("DOB"));
-			
+
 			TableColumn<Student, String> majorColumn = new TableColumn<>("Major");
 			majorColumn.setMinWidth(100);
 			majorColumn.setCellValueFactory(new PropertyValueFactory<>("major"));
-			
+
 			TableColumn<Student, String> genderColumn = new TableColumn<>("Gender");
 			genderColumn.setMinWidth(100);
 			genderColumn.setCellValueFactory(new PropertyValueFactory<>("gender"));
-			
+
 			TableColumn<Student, Double> GPAColumn = new TableColumn<>("GPA");
 			GPAColumn.setMinWidth(100);
 			GPAColumn.setCellValueFactory(new PropertyValueFactory<>("GPA"));
-			
+
 			TableColumn<Student, Double> totalUnitsColumn = new TableColumn<>("TotalUnits");
 			totalUnitsColumn.setMinWidth(100);
 			totalUnitsColumn.setCellValueFactory(new PropertyValueFactory<>("totalUnits"));
-			
+
 			TableColumn<Student, Double> dueTuitionColumn = new TableColumn<>("DueTuition");
 			dueTuitionColumn.setMinWidth(100);
 			dueTuitionColumn.setCellValueFactory(new PropertyValueFactory<>("dueTuition"));
-			
+
 			RegisterJDBC.excuteSQL(sql);
-			
+
 			studentTable.setItems(getStudent());
 			studentTable.getColumns().addAll(idColumn, firstNameColumn, lastNameColumn, addressColumn,
-					stateColumn, zipCodeColumn, SSNColumn, DOBColumn, majorColumn, genderColumn, 
+					stateColumn, zipCodeColumn, SSNColumn, DOBColumn, majorColumn, genderColumn,
 					GPAColumn, totalUnitsColumn, dueTuitionColumn);
 		}
 		else if(instructorRadio.isSelected()){
@@ -198,9 +198,9 @@ public class AdministerCenterController {
 	public void addStudentButtonListener() throws SQLException {
 		String sql = "INSERT INTO Student(StudentID, FirstName, LastName, Address, City, State,"
 				+ "ZipCode, SSN, DOB, Major, Gender, GPA, TotalUnits, DueTuition) VALUES"
-				+ "('" + studentID.getText() + "'," + "'" + firstName.getText() + "'," + "'" + lastName.getText() + "'," 
-				+ "'" + address.getText() + "'," + "'" + city.getText() + "'," + "'" + state.getText() + "'," 
-				+ "'" + zipCode.getText() + "'," + "'" + SSN.getText() + "'," + "'" + DOB.getText() + "'," 
+				+ "('" + studentID.getText() + "'," + "'" + firstName.getText() + "'," + "'" + lastName.getText() + "',"
+				+ "'" + address.getText() + "'," + "'" + city.getText() + "'," + "'" + state.getText() + "',"
+				+ "'" + zipCode.getText() + "'," + "'" + SSN.getText() + "'," + "'" + DOB.getText() + "',"
 				+ "'" + major.getText() + "'," + "'" + gender.getText() + "'," + "'" + Double.parseDouble(GPA.getText()) + "',"
 				+ "'" + Double.parseDouble(totalUnits.getText()) + "',"+ "'" + Double.parseDouble(dueTuition.getText()) + "')";
 		RegisterJDBC.excuteSQL(sql);
@@ -211,34 +211,35 @@ public class AdministerCenterController {
 		String sql = "DELETE FROM Student WHERE StudentID =" + "'" + studentID.getText() + "'";
 		RegisterJDBC.excuteSQL(sql);
 	}
-	
+
 	// Button listener for adding new course into database with information from Textfield
 	public void addCourseButtonListener() {
 
 	}
 
-	// Buttion listener for deleting a student from the database.
+	// Button listener for deleting a student from the database.
 	public void deleteCourseButtonListener() throws SQLException {
 		String sql = "DELETE FROM Course WHERE CourseCode =" + "'" + courseCode.getText() + "'";
 		RegisterJDBC.excuteSQL(sql);
 	}
-	
+
 	// Button listener for executing a SQL statement.
 	public void excuteSQLButtonListener() throws SQLException {
+		System.out.println(SQLArea.getText());
 		RegisterJDBC.excuteSQL(SQLArea.getText());
 	}
-	
+
 	ObservableList<Student> getStudent() throws SQLException{
 		ObservableList<Student> students = FXCollections.observableArrayList();
 		while(RegisterJDBC.result.next()){
-		students.add(new Student(RegisterJDBC.result.getString("StudentID"), RegisterJDBC.result.getString("FirstName"), 
-				RegisterJDBC.result.getString("LastNAME"), RegisterJDBC.result.getString("Address"), 
-				RegisterJDBC.result.getString("City"), RegisterJDBC.result.getString("State"), 
+		students.add(new Student(RegisterJDBC.result.getString("StudentID"), RegisterJDBC.result.getString("FirstName"),
+				RegisterJDBC.result.getString("LastNAME"), RegisterJDBC.result.getString("Address"),
+				RegisterJDBC.result.getString("City"), RegisterJDBC.result.getString("State"),
 				RegisterJDBC.result.getString("ZipCode"), RegisterJDBC.result.getString("SSN"),
-				RegisterJDBC.result.getString("DOB"), RegisterJDBC.result.getString("Major"), 
-				RegisterJDBC.result.getString("Gender"), 
-				Double.parseDouble(RegisterJDBC.result.getString("GPA")), 
-				Double.parseDouble(RegisterJDBC.result.getString("FurtureTuition")), 
+				RegisterJDBC.result.getString("DOB"), RegisterJDBC.result.getString("Major"),
+				RegisterJDBC.result.getString("Gender"),
+				Double.parseDouble(RegisterJDBC.result.getString("GPA")),
+				Double.parseDouble(RegisterJDBC.result.getString("FurtureTuition")),
 				Double.parseDouble(RegisterJDBC.result.getString("DueTuition")),
 				Double.parseDouble(RegisterJDBC.result.getString("TotalUnits")),
 				Double.parseDouble(RegisterJDBC.result.getString("CurrentUnits")),
@@ -246,13 +247,13 @@ public class AdministerCenterController {
 		}
 		return students;
 	}
-	
+
 	ObservableList<Instructor> getInstructor(){
 		ObservableList<Instructor> instructors = FXCollections.observableArrayList();
 		instructors.add(new Instructor());
 		return instructors;
 	}
-	
+
 	ObservableList<Course> getCourse(){
 		ObservableList<Course> courses = FXCollections.observableArrayList();
 		courses.add(new Course());
--- RegisterSystem.java
+++ RegisterSystem.java
@@ -1,6 +1,5 @@
 package Register;
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,20 +8,24 @@ import javafx.stage.Stage;
 
 public class RegisterSystem extends Application{
 	Stage window;
-	
 	@Override
 	public void start(Stage stage){
 		window = stage;
 		Parent parent;
 		try {
+			RegisterJDBC.connect();
 			parent = FXMLLoader.load(getClass().getResource("MainFX.fxml"));
 			Scene scene = new Scene(parent);
-			
+
 			window.setTitle("Register Sytem");
 			window.setScene(scene);
 			window.show();
-			
+
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
